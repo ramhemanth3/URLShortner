e084e2a1c73425c562690a28199bae080bab273e
ramhemanth3 <ram.hemanth3@gmail.com>
2020-04-23 00:54:09 +0530

    Commit before ending

--- Shortner/models.py
+++ Shortner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class URLData(models.Model):
+	URLID=models.CharField(max_length=1000)
+	ShortURL=model.CharField(max_length=1000)
+	
+	def _str_(self):
+		template = '{0.URLID}, {0.ShortURL}'
+		return template.format(self)
+		
+		
